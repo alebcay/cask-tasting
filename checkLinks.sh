1a66549ba819f5cc3885a806e545a02637d57218
Caleb Xu <calebcenter@live.com>
2014-03-22 20:29:50 -0400

    Add status message

--- checkLinks.sh
+++ checkLinks.sh
@@ -2,6 +2,7 @@ brew detox
 FILES=/usr/local/Library/Taps/phinze-cask/Casks/*.rb
 for f in $FILES
 do
+  echo "==> Downloading and checking ${f%.*}"
   brew cask audit --download "${f%.*}"
 done
 brew detox
