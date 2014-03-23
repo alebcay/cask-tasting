3afd5dfc273bc4b0ff57360e0acb3ccb33d7c1d1
Caleb Xu <calebcenter@live.com>
2014-03-22 20:10:30 -0400

    Switch to also downloading and checking checksums

--- checkLinks.sh
+++ checkLinks.sh
@@ -1,3 +1,7 @@
-brew update
-brew upgrade
-/usr/local/bin/brew cask checklinks
+brew detox
+FILES=/usr/local/Library/Taps/phinze-cask/Casks/*.rb
+for f in $FILES
+do
+  brew cask audit --download "${f%.*}"
+done
+brew detox
