69ff85274b718d37a7ca5c76215dcf99a7b38e8d
Caleb Xu <calebcenter@live.com>
2014-03-27 18:10:00 -0400

    Count the number of Casks that pass, fail...

--- checkLinks.sh
+++ checkLinks.sh
@@ -10,6 +10,10 @@ TOTAL=$( ls -1 /usr/local/Library/Taps/phinze-cask/Casks | wc -l | sed -e 's/^ *
 # TOTAL=$(echo $TOTALSTRING | sed -E 's/^.{4}//')
 FILES=/usr/local/Library/Taps/phinze-cask/Casks/*.rb
 counter=0
+passed=0
+dlfailed=0
+sumfailed=0
+nosum=0
 echo "Check started at $(date)" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
 echo "Check started at $(date)"
 for f in $FILES
@@ -43,15 +47,18 @@ do
     if [ "$EXPECTED_SHA" = "$ACTUAL_SHA" ]
       then
       echo -e "$(basename ${f%.*}) ($counter/$TOTAL): \033[1;32mpassed\033[22;0m"
+      ((passed++))
       echo -e "$(basename ${f%.*}): passed" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
       echo "$(basename ${f%.*})" >> /Volumes/SHARED/Dropbox/Public/CaskPassed.part
     elif [ "$ACTUAL_SHA" = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" ]
       then
       echo -e "$(basename ${f%.*}) ($counter/$TOTAL): \033[1;31mdownload error\033[22;0m"
+      ((dlfailed++))
       echo -e "$(basename ${f%.*}): download error" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
       echo "$(basename ${f%.*})" >> /Volumes/SHARED/Dropbox/Public/CaskDLFailed.part
     else
       echo -e "$(basename ${f%.*}) ($counter/$TOTAL): \033[1;31mSHA-$SHA_ALG mismatch!\033[22;0m"
+      ((sumfailed++))
       echo -e "$(basename ${f%.*}): SHA-$SHA_ALG mismatch!" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
       echo -e "  Expected: $EXPECTED_SHA" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
       echo -e "  Actual  : $ACTUAL_SHA" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
@@ -64,12 +71,13 @@ do
   elif [ "$SHA_ALG" = "NONE" ]
     then
     echo -e "$(basename ${f%.*}) ($counter/$TOTAL): \033[1;34mno checksum\033[22;0m"
+    ((nosum++))
     echo -e "$(basename ${f%.*}): no checksum" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
     echo "$(basename ${f%.*})" >> /Volumes/SHARED/Dropbox/Public/CaskNoSum.part
   fi
 done
 echo "Check finished at $(date)" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
-echo "Check finished at $(date)"
+echo "Check finished at $(date) ($passed passed, $dlfailed failed to download, $sumfailed had checksum mismatch, $nosum had no defined expected checksum)"
 rm Testfile
 mv /Volumes/SHARED/Dropbox/Public/CaskTasting.part /Volumes/SHARED/Dropbox/Public/CaskTasting.txt
 mv /Volumes/SHARED/Dropbox/Public/CaskPassed.part /Volumes/SHARED/Dropbox/Public/CaskPassed.txt
