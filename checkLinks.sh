a5ae2d0ad649c76ffcf5b6ec821fe17c58064634
Caleb Xu <calebcenter@live.com>
2014-03-29 18:47:11 -0400

    Add webpage

--- checkLinks.sh
+++ checkLinks.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-cd /Volumes/MacData/homebrew/cask-tasting
 echo "Preparing for check..."
 brew detox >/dev/null 2>&1
-[ -d "CaskTasting.part" ] && rm "CaskTasting.part"
-[ -d "CaskPassed.part" ] && rm "CaskPassed.part"
-[ -d "CaskFailed.part" ] && rm "CaskFailed.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskTasting.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskTasting.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskPassed.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskPassed.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskFailed.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskFailed.part"
 TOTAL=$( ls -1 /usr/local/Library/Taps/phinze-cask/Casks | wc -l | sed -e 's/^ *//' -e 's/ *$//')
 # TOTAL=$(echo $TOTALSTRING | sed -E 's/^.{4}//')
 FILES=/usr/local/Library/Taps/phinze-cask/Casks/*.rb
 counter=0
 passed=0
 failed=0
-echo "Check started at $(date)" >> CaskTasting.part
+echo "Check started at $(date)" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
 echo "Check started at $(date)"
 for f in $FILES
 do
@@ -21,22 +20,22 @@ do
   if [ "$?" = 0 ]
     then
     ((passed++))
-    echo "${2%.*}: passed" >> CaskPassed.part
+    echo "${2%.*}: passed" >> /Volumes/SHARED/Dropbox/Public/CaskPassed.part
   else
     ((failed++))
-    echo "${2%.*}: passed" >> CaskFailed.part
+    echo "${2%.*}: passed" >> /Volumes/SHARED/Dropbox/Public/CaskFailed.part
   fi
 done
-echo "Check finished at $(date)" >> CaskTasting.part
+echo "Check finished at $(date)" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
 echo "Check finished at $(date) ($passed \033[1;32mpassed\033[22;0m, $failed \033[1;31mfailed\033[22;0m)"
-mv CaskTasting.txt history/CaskTasting-$(date +%F-%T).txt
-mv CaskPassed.txt history/CaskPassed-$(date +%F-%T).txt
-mv CaskFailed.txt history/CaskFailed-$(date +%F-%T).txt
+mv /Volumes/SHARED/Dropbox/Public/CaskTasting.txt /Volumes/SHARED/Dropbox/Public/CaskHistory/CaskTasting-$(date +%F-%T).txt
+mv /Volumes/SHARED/Dropbox/Public/CaskPassed.txt /Volumes/SHARED/Dropbox/Public/CaskHistory/CaskPassed-$(date +%F-%T).txt
+mv /Volumes/SHARED/Dropbox/Public/CaskFailed.txt /Volumes/SHARED/Dropbox/Public/CaskHistory/CaskFailed-$(date +%F-%T).txt
 
-mv CaskTasting.part CaskTasting.txt
-mv CaskPassed.part CaskPassed.txt
-mv CaskFailed.part CaskFailed.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskTasting.part /Volumes/SHARED/Dropbox/Public/CaskTasting.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskPassed.part /Volumes/SHARED/Dropbox/Public/CaskPassed.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskFailed.part /Volumes/SHARED/Dropbox/Public/CaskFailed.txt
 
-rm CaskTasting.part
-rm CaskPassed.part
-rm CaskFailed.part
+rm /Volumes/SHARED/Dropbox/Public/CaskTasting.part
+rm /Volumes/SHARED/Dropbox/Public/CaskPassed.part
+rm /Volumes/SHARED/Dropbox/Public/CaskFailed.part
