9246282854d91b60879b07328835033924332b31
Caleb Xu <calebcenter@live.com>
2014-03-31 23:15:08 -0400

    Finishing up on conflict

--- checkLinks.sh
+++ checkLinks.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 doalarm() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
-=======
-cd /Volumes/MacData/homebrew/cask-tasting
->>>>>>> FETCH_HEAD
 echo "Preparing for check..."
 brew detox >/dev/null 2>&1
-[ -d "CaskTasting.part" ] && rm "CaskTasting.part"
-[ -d "CaskPassed.part" ] && rm "CaskPassed.part"
-[ -d "CaskFailed.part" ] && rm "CaskFailed.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskTasting.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskTasting.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskPassed.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskPassed.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskDLError.part" ] && rm "//Volumes/SHARED/Dropbox/Public/CaskDLError.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskSumError.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskSumError.part"
+[ -d "/Volumes/SHARED/Dropbox/Public/CaskNoSum.part" ] && rm "/Volumes/SHARED/Dropbox/Public/CaskNoSum.part"
 TOTAL=$( ls -1 /usr/local/Library/Taps/phinze-cask/Casks | wc -l | sed -e 's/^ *//' -e 's/ *$//')
 # TOTAL=$(echo $TOTALSTRING | sed -E 's/^.{4}//')
 FILES=/usr/local/Library/Taps/phinze-cask/Casks/*.rb
 counter=0
-passed=0
-failed=0
-echo "Check started at $(date)" >> CaskTasting.part
+echo "Check started at $(date)" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
 echo "Check started at $(date)"
 for f in $FILES
 do
   ((counter++))
-<<<<<<< HEAD
   [ -f "Testfile" ] && rm Testfile
   while read line
   do
@@ -76,29 +71,12 @@ do
     echo -e "$(basename ${f%.*}) ($counter/$TOTAL): \033[1;34mno checksum\033[22;0m"
     echo -e "$(basename ${f%.*}): no checksum" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
     echo "$(basename ${f%.*})" >> /Volumes/SHARED/Dropbox/Public/CaskNoSum.part
-=======
-  echo -e "\033[1;33mTesting $counter/$TOTAL\033[22;0m"
-  brew cask audit --download $f
-  if [ "$?" = 0 ]
-    then
-    ((passed++))
-    echo "${2%.*}: passed" >> CaskPassed.part
-  else
-    ((failed++))
-    echo "${2%.*}: passed" >> CaskFailed.part
->>>>>>> FETCH_HEAD
   fi
 done
-echo "Check finished at $(date)" >> CaskTasting.part
-echo "Check finished at $(date) ($passed \033[1;32mpassed\033[22;0m, $failed \033[1;31mfailed\033[22;0m)"
-mv CaskTasting.txt history/CaskTasting-$(date +%F-%T).txt
-mv CaskPassed.txt history/CaskPassed-$(date +%F-%T).txt
-mv CaskFailed.txt history/CaskFailed-$(date +%F-%T).txt
-
-mv CaskTasting.part CaskTasting.txt
-mv CaskPassed.part CaskPassed.txt
-mv CaskFailed.part CaskFailed.txt
-
-rm CaskTasting.part
-rm CaskPassed.part
-rm CaskFailed.part
+echo "Check finished at $(date)" >> /Volumes/SHARED/Dropbox/Public/CaskTasting.part
+echo "Check finished at $(date)"
+mv /Volumes/SHARED/Dropbox/Public/CaskTasting.part /Volumes/SHARED/Dropbox/Public/CaskTasting.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskPassed.part /Volumes/SHARED/Dropbox/Public/CaskPassed.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskDLFailed.part /Volumes/SHARED/Dropbox/Public/CaskDLFailed.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskSumError.part /Volumes/SHARED/Dropbox/Public/CaskSumError.txt
+mv /Volumes/SHARED/Dropbox/Public/CaskNoSum.part /Volumes/SHARED/Dropbox/Public/CaskNoSum.txt
