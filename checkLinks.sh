c5c0f3401fe18115a360d606d2e15c53943eb257
Caleb Xu <calebcenter@live.com>
2014-11-06 12:52:18 -0500

    Cask taster reporting for duty: Thu Nov  6 12:52:18 EST 2014

--- checkLinks.sh
+++ checkLinks.sh
@@ -14,7 +14,7 @@ echo "Check started at $STARTTIME"
 #FILES=/usr/local/Library/Taps/caskroom/homebrew-cask/Casks/*.rb
 
 #parallel -k --progress ./parallelcheck.sh {} ::: $FILES
-ppss -p 10 -d /usr/local/Library/Taps/caskroom/homebrew-cask/Casks -c './parallelcheck.sh '
+ppss -d /usr/local/Library/Taps/caskroom/homebrew-cask/Casks -c './parallelcheck.sh '
 
 sort ./CaskTasting.part -o ./CaskTasting.part
 echo "Check started at $STARTTIME" | cat - ./CaskTasting.part > tmp.txt
